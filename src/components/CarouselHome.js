8d974c193722d18ee65a588975f09b71f52b6b5c
Haithem-MERN <dev.fsjs@gmail.com>
2021-12-01 11:30:15 +0100

    last commit
    Project Done !

--- src/components/CarouselHome.js
+++ src/components/CarouselHome.js
@@ -1,50 +1,35 @@
 import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
-import imgM1 from '../images/musc/DSC_0755.JPG';
-import imgM2 from '../images/musc/DSC_0768.JPG';
-import imgM3 from '../images/musc/DSC_0759.JPG';
-import imgM4 from '../images/musc/DSC_0764.JPG';
-
+import imgM1 from "../images/musc/DSC_0755.png";
+import imgM2 from "../images/musc/DSC_0768.png";
+import imgM3 from "../images/musc/DSC_0759.png";
+import imgM4 from "../images/musc/DSC_0764.png";
 
 const CarouselHome = () => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    }
-    return (
-        <div id="carousselHome">
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <div id="carousselHome">
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
-          <img
-            src={imgM1}
-            alt="First slide"
-          />
+          <img src={imgM1} alt="First slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <img
-            src={imgM2}
-            alt="First slide"
-          />
+          <img src={imgM2} alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            src={imgM3}
-            alt="First slide"
-          />
+          <img src={imgM3} alt="First slide" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            src={imgM4}
-            alt="First slide"
-          />
+          <img src={imgM4} alt="First slide" />
         </Carousel.Item>
-         
       </Carousel>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-
-export default CarouselHome
+export default CarouselHome;
--- src/pages/Acceuil.js
+++ src/pages/Acceuil.js
@@ -13,53 +13,51 @@ const Acceuil = () => {
   }, []);
 
   return (
-    <div className="landing">
-      <div className="parallax">
-        <section data-aos="fade-down" className="sect1">
-          <div className="sect1content">
-            <h1>BIENVENUE À Mayar Golden Gym</h1>
-            <div className="front-img">
-              <img src={logo_a} alt="log mayar"></img>
-            </div>
-            <h3> Salle de sport et de Fitness </h3>
-            <div className="btn-sect1">
-              <Link className="btn_Club" to="/espace-musculation">
-                Club
-              </Link>
-              <Link className="btn-contact" to="/contact">
-                Contact
-              </Link>
-            </div>
+    <div className="parallax">
+      <section data-aos="fade-down" className="sect1">
+        <div className="sect1content">
+          <h1>BIENVENUE À Mayar Golden Gym</h1>
+          <div className="front-img">
+            <img src={logo_a} alt="log mayar"></img>
           </div>
-        </section>
-        <section className="sect2">
-          <div className="sect2Content" data-aos="fade-right">
-            <h1>Consulter Nos tarifs</h1>
-            <Link className="btn-2" to="/tarifs">
-              Nos Tarifs
+          <h3> Salle de sport et de Fitness </h3>
+          <div className="btn-sect1">
+            <Link className="btn_Club" to="/espace-musculation">
+              Club
+            </Link>
+            <Link className="btn-contact" to="/contact">
+              Contact
             </Link>
           </div>
-        </section>
-        <section className="sect3">
-          <div data-aos="fade-right" className="sect3Content">
-            <h1>Qui sommes Nous ?</h1>
-            <div className="descirption">
-              <p>
-                Profitez d'un cadre agréable, des espaces aérés et climatisés,
-                des équipements de pointe et des coachs qualifiés pour vous
-                encadrer, vous motiver et vous soutenir. Mayar Golden Gym est
-                une salle de sport et de fitness qui vous propose des cours
-                variés dans un espace de Fitness destiné aux adultes, ados et
-                enfants. Vous trouverez tous ce qu'il vous faut dans nos espaces
-                de musculation et Cardio Training avec un matériel de qualité.
-              </p>
-            </div>
-            <div className="imgSlide">
-              <CarouselHome />
-            </div>
+        </div>
+      </section>
+      <section className="sect2">
+        <div className="sect2Content" data-aos="fade-right">
+          <h1>Consulter Nos tarifs</h1>
+          <Link className="btn-2" to="/tarifs">
+            Nos Tarifs
+          </Link>
+        </div>
+      </section>
+      <section className="sect3">
+        <div data-aos="fade-right" className="sect3Content">
+          <h1>Qui sommes Nous ?</h1>
+          <div className="descirption">
+            <p>
+              Profitez d'un cadre agréable, des espaces aérés et climatisés, des
+              équipements de pointe et des coachs qualifiés pour vous encadrer,
+              vous motiver et vous soutenir. Mayar Golden Gym est une salle de
+              sport et de fitness qui vous propose des cours variés dans un
+              espace de Fitness destiné aux adultes, ados et enfants. Vous
+              trouverez tous ce qu'il vous faut dans nos espaces de musculation
+              et Cardio Training avec un matériel de qualité.
+            </p>
           </div>
-        </section>
-      </div>
+          <div className="imgSlide">
+            <CarouselHome />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
